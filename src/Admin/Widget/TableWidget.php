d457bedab7705a0f1989cc06ed77b6ee7d69d559
Chauncey McAskill <chauncey@locomotive.ca>
2016-08-01 16:04:39 -0400

    Fixed attr and type options for TableWidget

--- src/Admin/Widget/TableWidget.php
+++ src/Admin/Widget/TableWidget.php
@@ -22,22 +22,62 @@ class TableWidget extends CharcoalTableWidget
     use HtmlableTrait;
 
     /**
+     * Retrieve the widget type.
+     *
+     * @return string
+     */
+    public function type()
+    {
+        return 'charcoal/admin/widget/table';
+    }
+
+    /**
      * Properties to display in collection template, and their order, as set in object metadata
      *
      * @return  FormPropertyWidget         Generator function
      */
     public function collectionProperties()
     {
-        $columns = parent::collectionProperties();
+        $props = $this->properties();
 
-        foreach ($columns as $column) {
+        foreach ($props as $propertyIdent => $property) {
+            $propertyMetadata = $props[$propertyIdent];
+
+            $p = $this->propertyFactory()->create($propertyMetadata['type']);
+            $p->setIdent($propertyIdent);
+            $p->setData($propertyMetadata);
+
+            $options = $this->viewOptions($propertyIdent);
+            $classes = $this->parsePropertyCellClasses($p);
+
+            $column = [
+                'label' => trim($p->label())
+            ];
+
             if (!isset($column['attr'])) {
                 $column['attr'] = [];
             }
 
-            if (isset($column['classes'])) {
-                $column['attr']['class'] = $column['classes'];
-                unset($column['classes']);
+            if (isset($options['attr'])) {
+                $column['attr'] = array_merge($column['attr'], $options['attr']);
+            }
+
+            if (isset($classes)) {
+                if (isset($column['attr']['class'])) {
+                    if (is_string($classes)) {
+                        $classes = explode(' ', $column['attr']['class']);
+                    }
+
+                    if (is_string($column['attr']['class'])) {
+                        $column['attr']['class'] = explode(' ', $column['attr']['class']);
+                    }
+
+                    $column['attr']['class'] = array_unique(array_merge($column['attr']['class'], $classes));
+                } else {
+                    $column['attr']['class'] = $classes;
+                }
+
+                unset($classes);
             }
 
             $column['attr'] = $this->htmlAttributes($column['attr']);
@@ -61,15 +101,35 @@ class TableWidget extends CharcoalTableWidget
         PropertyInterface $property,
         $propertyValue
     ) {
-        $cell = parent::parsePropertyCell($object, $property, $propertyValue);
+        $cell    = $this->parseCollectionPropertyCell($object, $property, $propertyValue);
+        $ident   = $property->ident();
+        $options = $this->viewOptions($ident);
+        $classes = $this->parsePropertyCellClasses($property, $object);
 
         if (!isset($cell['attr'])) {
             $cell['attr'] = [];
         }
 
-        if (isset($cell['classes'])) {
-            $cell['attr']['class'] = $cell['classes'];
-            unset($cell['classes']);
+        if (isset($options['attr'])) {
+            $cell['attr'] = array_merge($cell['attr'], $options['attr']);
+        }
+
+        if (isset($classes)) {
+            if (isset($cell['attr']['class'])) {
+                if (is_string($classes)) {
+                    $classes = explode(' ', $cell['attr']['class']);
+                }
+
+                if (is_string($cell['attr']['class'])) {
+                    $cell['attr']['class'] = explode(' ', $cell['attr']['class']);
+                }
+
+                $cell['attr']['class'] = array_unique(array_merge($cell['attr']['class'], $classes));
+            } else {
+                $cell['attr']['class'] = $classes;
+            }
+
+            unset($classes);
         }
 
         $cell['attr'] = $this->htmlAttributes($cell['attr']);
