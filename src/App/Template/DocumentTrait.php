695699db38cbfa55af77a91764edcd72c41f94c5
Chauncey McAskill <chauncey@locomotive.ca>
2016-11-23 14:13:06 -0500

    Added Document Trait for Templating
    Features:
    - Site Name;
    - Document Title (separator, mergeable title segments);

---
+++ src/App/Template/DocumentTrait.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Charcoal\Support\App\Template;
+
+// From 'charcoal-core'
+use Charcoal\Model\ModelInterface;
+
+/**
+ * Additional utilities for the HTML document.
+ */
+trait DocumentTrait
+{
+    /**
+     * Retrieve the site name.
+     *
+     * @return string|null
+     */
+    public function siteName()
+    {
+        return 'Charcoal Project';
+    }
+
+    /**
+     * Parse the document title parts.
+     *
+     * @return string[]
+     */
+    protected function documentTitleParts()
+    {
+        return [
+            'title' => $this->title(),
+            'site'  => $this->siteName(),
+        ];
+    }
+
+    /**
+     * Retrieve the document title separator.
+     *
+     * @return string
+     */
+    protected function documentTitleSeparator()
+    {
+        return '—';
+    }
+
+    /**
+     * Retrieve the document title.
+     *
+     * @return string
+     */
+    final public function documentTitle()
+    {
+        $parts = array_merge([ 'title' => '', 'site' => '' ], $this->documentTitleParts());
+        $parts = array_filter($parts, function ($segment, $key) use ($parts) {
+            if ($key !== 'site') {
+                return (false === strpos($segment, $parts['site']));
+            }
+
+            return $segment;
+        }, ARRAY_FILTER_USE_BOTH);
+        $delim = sprintf(' %s ', $this->documentTitleSeparator());
+        $title = implode($delim, $parts);
+
+        return $title;
+    }
+
+    /**
+     * Retrieve the title of the page (from the context).
+     *
+     * @return string
+     */
+    abstract public function title();
+
+    /**
+     * Retrieve the current object relative to the context.
+     *
+     * @return \Charcoal\Model\ModelInterface
+     */
+    abstract public function contextObject();
+}
