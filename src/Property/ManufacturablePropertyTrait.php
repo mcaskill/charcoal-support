10dc3297603aaeb2d8f906ae18d41326a16e201f
Joel Alphonso <joel@locomotive.ca>
2018-04-09 09:31:46 -0400

    Add ManufacturablePropertyTrait::createPropertyFromMetadata

--- src/Property/ManufacturablePropertyTrait.php
+++ src/Property/ManufacturablePropertyTrait.php
@@ -47,4 +47,56 @@ trait ManufacturablePropertyTrait
 
         return $this->propertyFactory;
     }
+
+    /**
+     * Create an instance of {@see PropertyInterface} for the given property.
+     *
+     * @param  string            $propertyIdent The property identifier to return.
+     * @param  MetadataInterface $metadata      The metadata to create the property from.
+     * @throws \InvalidArgumentException If the $propertyIdent is not a string.
+     * @throws \RuntimeException If the requested property is invalid.
+     * @return PropertyInterface The {@see PropertyInterface} if found, null otherwise
+     */
+    protected function createPropertyFromMetadata($propertyIdent, MetadataInterface $metadata)
+    {
+        if (!is_string($propertyIdent)) {
+            throw new \InvalidArgumentException(
+                'Property identifier must be a string.'
+            );
+        }
+
+        $props = $metadata->properties();
+
+        if (empty($props)) {
+            throw new \RuntimeException(sprintf(
+                'Invalid model metadata [%s] - No properties defined.',
+                get_class($this)
+            ));
+        }
+
+        if (!isset($props[$propertyIdent])) {
+            throw new \RuntimeException(sprintf(
+                'Invalid model metadata [%s] - Undefined property metadata for "%s".',
+                get_class($this),
+                $propertyIdent
+            ));
+        }
+
+        $propertyMetadata = $props[$propertyIdent];
+        if (!isset($propertyMetadata['type'])) {
+            throw new \RuntimeException(sprintf(
+                'Invalid model metadata [%s] - Undefined property type for "%s".',
+                get_class($this),
+                $propertyIdent
+            ));
+        }
+
+        $factory  = $this->propertyFactory();
+        $property = $factory->create($propertyMetadata['type']);
+        $property->metadata();
+        $property->setIdent($propertyIdent);
+        $property->setData($propertyMetadata);
+
+        return $property;
+    }
 }
