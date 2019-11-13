38b26d2301b1ba655b02fd9cae9c0721d1aeb348
Chauncey McAskill <chauncey@locomotive.ca>
2019-11-13 15:40:23 -0500

    Update naming conventions in AbstractSettings
    Update class to latest version of locomotivemtl/charcoal-config v0.10+

--- src/Cms/Object/AbstractSettings.php
+++ src/Cms/Object/AbstractSettings.php
@@ -33,24 +33,31 @@ class AbstractSettings extends Content implements
     {
         if (is_numeric($key)) {
             throw new InvalidArgumentException(
-                'Entity array access only supports non-numeric keys.'
+                'Settings array access only supports non-numeric keys'
             );
         }
 
         $key = $this->camelize($key);
+
+        /** @internal Edge Case: "_" → "" */
         if ($key === '') {
             return false;
         }
 
-        if (is_callable([ $this, $key ])) {
-            $value = $this->{$key}();
-        } else {
-            if (!isset($this->{$key})) {
-                return $this->hasInDelegates($key);
-            }
-            $value = $this->{$key};
+        $getter = 'get'.ucfirst($key);
+        if (!isset($this->mutatorCache[$getter])) {
+            $this->mutatorCache[$getter] = is_callable([ $this, $getter ]);
         }
-        return ($value !== null);
+
+        if ($this->mutatorCache[$getter]) {
+            return ($this->{$getter}() !== null);
+        }
+
+        if (isset($this->{$key})) {
+            return true;
+        }
+
+        return $this->hasInDelegates($key);
     }
 
     /**
@@ -65,32 +72,35 @@ class AbstractSettings extends Content implements
     {
         if (is_numeric($key)) {
             throw new InvalidArgumentException(
-                'Entity array access only supports non-numeric keys.'
+                'Settings array access only supports non-numeric keys'
             );
         }
 
         $key = $this->camelize($key);
+
+        /** @internal Edge Case: "_" → "" */
         if ($key === '') {
             return null;
         }
 
-        if (is_callable([ $this, $key ])) {
-            return $this->{$key}();
-        } else {
-            if (isset($this->{$key})) {
-                return $this->{$key};
-            } else {
-                return $this->getInDelegates($key);
-            }
+        $getter = 'get'.ucfirst($key);
+        if (!isset($this->mutatorCache[$getter])) {
+            $this->mutatorCache[$getter] = is_callable([ $this, $getter ]);
         }
+
+        if ($this->mutatorCache[$getter]) {
+            return $this->{$getter}();
+        }
+
+        if (isset($this->{$key})) {
+            return $this->{$key};
+        }
+
+        return $this->getInDelegates($key);
     }
 
     /**
      * Assign a value to the specified key of the configuration.
-     *
-     * Set the value either by:
-     * - a setter method (`set_{$key}()`)
-     * - setting (or overriding)
      *
      * @see    \ArrayAccess::offsetSet()
      * @param  string $key   The key to assign $value to.
@@ -102,35 +112,82 @@ class AbstractSettings extends Content implements
     {
         if (is_numeric($key)) {
             throw new InvalidArgumentException(
-                'Entity array access only supports non-numeric keys.'
+                'Settings array access only supports non-numeric keys'
             );
         }
 
         $key = $this->camelize($key);
+
+        /** @internal Edge Case: "_" → "" */
         if ($key === '') {
             return;
         }
 
-        $setter = 'set' . ucfirst($key);
-        if (is_callable([ $this, $setter ])) {
+        $setter = 'set'.ucfirst($key);
+        if (!isset($this->mutatorCache[$setter])) {
+            $this->mutatorCache[$setter] = is_callable([ $this, $setter ]);
+        }
+
+        if ($this->mutatorCache[$setter]) {
             $this->{$setter}($value);
         } else {
             $this->{$key} = $value;
         }
 
-        $this->keys[$key] = true;
+        $this->keyCache[$key] = true;
     }
 
     /**
-     * Add data to settings, replacing existing values with the same data key.
+     * Replaces the value from the specified key.
      *
-     * @param  array|\Traversable $data Datas to merge.
-     * @return SettingsInterface
+     * Routine:
+     * - When the value in the Config and the new value are both arrays,
+     *   the method will replace their respective value recursively.
+     * - Then or otherwise, the new value is {@see self::offsetSet() assigned} to the Config.
+     *
+     * @uses   \Charcoal\Config\AbstractConfig::offsetReplace()
+     * @uses   array_replace_recursive()
+     * @param  string $key   The data key to assign or merge $value to.
+     * @param  mixed  $value The data value to assign to or merge with $key.
+     * @throws InvalidArgumentException If the $key is not a string or is a numeric value.
+     * @return void
+     */
+    public function offsetReplace($key, $value)
+    {
+        if (is_numeric($key)) {
+            throw new InvalidArgumentException(
+                'Settings array access only supports non-numeric keys'
+            );
+        }
+
+        $key = $this->camelize($key);
+
+        /** @internal Edge Case: "_" → "" */
+        if ($key === '') {
+            return;
+        }
+
+        if (is_array($value) && isset($this[$key])) {
+            $data = $this[$key];
+            if (is_array($data)) {
+                $value = array_replace_recursive($data, $value);
+            }
+        }
+
+        $this[$key] = $value;
+    }
+
+    /**
+     * Adds new data, replacing / merging existing data with the same key.
+     *
+     * @see    \Charcoal\Config\AbstractConfig::merge()
+     * @param  array|\Traversable $data Key-value dataset to merge.
+     * @return self
      */
     public function merge($data)
     {
         foreach ($data as $key => $value) {
-            $this[$key] = $value;
+            $this->offsetReplace($key, $value);
         }
 
         return $this;
